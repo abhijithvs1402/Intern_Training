4d28630d45503f166be38a5fa0fa0d26cf92556d
ABHIJITH V S <89441680+abhijithvs1402@users.noreply.github.com>
2023-02-23 14:07:54 +0530

    Add files via upload

---
+++ Assessment 1/Arithmetic operators.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+//unaryoperators
+
+int main()
+
+{
+    int a=2, b=3;
+    printf("Given a=2 and b=3\n");
+    printf("a+b = %d \n",a+b);
+    printf("a-b = %d\n",a-b);
+    printf("a*b = %d\n",a*b);
+    printf("a/b = %d\n",a/b);
+    printf("b modulus a= %d ",b%a);
+
+    
+}
