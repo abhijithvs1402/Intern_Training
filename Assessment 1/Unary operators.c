5be06fab6424b06a0bd8588cc50b29e6a27f3ea1
ABHIJITH V S <89441680+abhijithvs1402@users.noreply.github.com>
2023-02-23 14:02:08 +0530

    Add files via upload

---
+++ Assessment 1/Unary operators.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+//unaryoperators
+
+int main()
+
+{
+    int a=2;
+    printf(" Given a=2 \n");
+
+    printf("a++ = %d\n",a++);
+    printf("a-- = %d\n",a--);
+    printf("++a = %d\n",++a);
+    printf("--a = %d\n",--a);
+    printf("not a is %d\n",!a);
+    printf("size of a is %d\n",sizeof(a));
+    printf("Address of a is %d \n",&a);
+    
+}
